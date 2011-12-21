4eee4aa12f24376337d9bb999fc65a92eae3b3d3
Chakrit Wichian <chakrit@oozou.com>
2011-12-22 02:44:53 +0700

    Update model.js with new JAM helpers.

--- model.js
+++ model.js
@@ -6,17 +6,17 @@
     , redis = require('redis')
     , client = redis.createClient();
 
-  // TODO: Add jam.map() and jam.return()
+  // will explain later :)
   var model =
     { 'verses':
       { 'find': function(id) {
           return jam(function() { redis.get('v:' + id, this); })
-            (function(json) { this(JSON.parse(raw)); });
+            .map(JSON.parse);
         }
       , 'save': function(verse) {
-          return jam(function() { this(JSON.stringify(verse)); })
+          return jam.return(JSON.stringify(verse))
             (function(json) { redis.set('v:' + id, json, this); })
-            (function(result) { this(result == "+OK"); });
+            (function(reuslt) { this(result === '+OK'); });
         } } };
 
   // post-processing
