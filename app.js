982f9cc65c4aebbf40c0f0c8dee6a003e2b7b7fa
Phatograph <phatograph@gmail.com>
2011-12-22 00:03:30 +0700

    Update package.json dependencies Add expiress configuration and view rendering using jade

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
-
 var express = require('express')
   , app = express.createServer();
 
-app.get('/', function(req, resp) {
-  resp.send("Coming soon!");
+app.configure(function(){
+    app.use(express.methodOverride());
+    app.use(express.bodyParser());
+    app.use(app.router);
+    app.set('view engine', 'jade');
+});
+
+app.configure('development', function(){
+    app.use(express.static(__dirname + '/public'));
+    app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function(){
+  var oneYear = 31557600000;
+  app.use(express.static(__dirname + '/public', { maxAge: oneYear }));
+  app.use(express.errorHandler());
+});
+
+app.get('/', function(req, res){
+  res.render('index', { title: 'My Site' });
 });
 
 var port = process.env.PORT || 1337;
 console.log("Listening on " + port);
 app.listen(port);
-
