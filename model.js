5a15ecd2994299dcf23eeec5be6d8cfa7e33201e
Chakrit Wichian <chakrit@oozou.com>
2011-12-24 01:29:29 +0700

    Add user functions.

--- model.js
+++ model.js
@@ -6,24 +6,32 @@
     , redis = require('redis')
     , client = redis.createClient();
 
+  // common redis extension to JAM
+  // TODO: Actually could make use of prototypes in JAM so we
+  //   can manipulate functions and add extensions/whatnot here
+  //   just like jQuery.fn === jQuery.prototype
+  var jx =
+    { 'isOk': function(result) { this(result === '+OK'); }
+    , 'asJson': function(result) { this(JSON.parse(result)); } };
+
   // will explain later :)
   var model =
     { 'verses':
       { 'find': function(id) {
-          return jam(function() { redis.get('v:' + id, this); })
-            .map(JSON.parse);
+          return jam.call(redis.get, 'v:' + id)(jx.asJson);
         }
       , 'save': function(verse) {
-          return jam.return(JSON.stringify(verse))
-            (function(json) { redis.set('v:' + id, json, this); })
-            (function(reuslt) { this(result === '+OK'); });
+          var json = JSON.stringify(verse);
+          return jam.call(redis.set, 'v:' + verse.id, json)(jx.isOk);
+        } }
+    , 'users':
+      { 'find': function(id) {
+          return jam.call(redis.get, 'u:' + id)(jx.asJson);
+        }
+      , 'save': function(user) {
+          var json = JSON.stringify(user);
+          return jam.call(redis.set, 'u:' + user.id, json)(jx.isOk);
         } } };
-
-  // post-processing
-  for (var key in model) {
-    var modelClass = model[key];
-    // apply any layer-wide stuff
-  }
 
   module.exports = model;
 
